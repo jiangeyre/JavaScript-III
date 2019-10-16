50c32050c5063f1ac5404dfef8be104c4c3e57cc
Lexie Jiang <LexieJiang@Lexies-MacBook-Pro.local>
2019-10-16 13:47:17 -0700

    basic set up for stretch problems - constructors and prototype methods

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -61,6 +61,30 @@ Humanoid.prototype.greet = function () {
   return `${this.name} offers a greeting in ${this.language}.`;
 };
 
+// * STRETCH : Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+// * Create two new objects, one a villain and one a hero and fight it out with methods!
+function Villain(villainAttr){
+  Humanoid.call(this, villainAttr);
+
+}
+Villain.prototype = Object.create(Humanoid.prototype);
+
+Villain.prototype.attackHero = function (){
+
+};
+
+function Hero(heroAttr){
+  Humanoid.call(this, heroAttr);
+
+}
+Hero.prototype = Object.create(Humanoid.prototype);
+
+Hero.prototype.attackVillain = function (){
+
+};
+
+
 
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
